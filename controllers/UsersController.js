7f162d610475bda8267909014e000012866981ac
Menna Hassan <h.menna85@gmail.com>
2024-05-13 15:37:54 +0300

    task 3

---
+++ controllers/UsersController.js
@@ -0,0 +1,40 @@
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+class UsersController {
+  static async postNew(req, res) {
+    const email = req.body.email || null;
+    const password = req.body.password || null;
+
+    if (!req.body) {
+      return res.json({ error: 'Missing request body' }).status(400);
+    }
+
+    if (!email) {
+      return res.json({ error: 'Missing email' }).status(400);
+    }
+
+    if (!password) {
+      return res.json({ error: 'Missing password' }).status(400);
+    }
+
+    const collection = dbClient.client.db().collection('users');
+
+    const user = await collection.findOne({ email });
+
+    if (user) {
+      return res.json({ error: 'Already exist' }).status(400);
+    }
+
+    const hashedPassword = sha1(password);
+
+    const userAdded = await collection.insertOne({
+      email,
+      password: hashedPassword,
+    });
+
+    return res.json({ id: userAdded.insertedId, email }).status(201);
+  }
+}
+
+export default UsersController;
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-import express from 'express';
+import Express from 'express';
 import router from './routes';
 
-const app = express();
+const app = new Express();
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log('Server running on port ', port);
 });
+app.use(Express.json());
 app.use('/', router);
