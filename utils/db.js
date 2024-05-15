e4e90edbe884f1f7635ca9310ad972358ef820de
yasmeen <yasmeenhany14@gmail.com>
2024-05-15 15:53:14 +0300

    saved changes

--- utils/db.js
+++ utils/db.js
@@ -3,7 +3,7 @@ import redisClient from './redis';
 
 class DBClient {
   constructor() {
-    this.host = process.env.DB_HOST || '0.0.0.0';
+    this.host = process.env.DB_HOST || 'localhost';
     this.port = process.env.DB_PORT || 27017;
     this.database = process.env.DB_DATABASE || 'files_manager';
     this.url = `mongodb://${this.host}:${this.port}/${this.database}`;
